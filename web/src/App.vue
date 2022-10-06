af90e8a915b1fd21a395b9eb238f6d00e1729a80
Daniel Honus <danielhonus@tutanota.com>
2022-10-06 15:22:56 +0200

    fonts round 2

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,13 +9,12 @@
 </style>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Raleway&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   font-family: 'Montserrat', sans-serif;
-  font-family: 'Raleway', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
