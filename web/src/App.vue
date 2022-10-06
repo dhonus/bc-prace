cb8cdf546963fd2ca9f2f2f038db3e1e731acb7c
Daniel Honus <danielhonus@tutanota.com>
2022-10-06 15:24:48 +0200

    vue is confusing

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,9 +7,7 @@
 <style lang="scss">
 @import "assets/style.sass";
 </style>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+
 
 <style>
 #app {
