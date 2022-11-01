705a4e095294710a8aeadd0a9eabc9360cd192c1
Daniel Honus <danielhonus@tutanota.com>
2022-11-01 08:04:50 +0100

    chartjs static

---
+++ web/chartjs-chart-venn-main/rollup.config.js
@@ -0,0 +1,132 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import cleanup from 'rollup-plugin-cleanup';
+import dts from 'rollup-plugin-dts';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from '@chiogen/rollup-plugin-terser';
+import replace from '@rollup/plugin-replace';
+import json from '@rollup/plugin-json';
+
+import fs from 'fs';
+
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
+
+function resolveYear() {
+  // Extract copyrights from the LICENSE.
+  const license = fs.readFileSync('./LICENSE', 'utf-8').toString();
+  const matches = Array.from(license.matchAll(/\(c\) (\d+)/gm));
+  if (!matches || matches.length === 0) {
+    return 2021;
+  }
+  return matches[matches.length - 1][1];
+}
+const year = resolveYear();
+
+const banner = `/**
+ * ${pkg.name}
+ * ${pkg.homepage}
+ *
+ * Copyright (c) ${year} ${pkg.author.name} <${pkg.author.email}>
+ */
+`;
+
+/**
+ * defines which formats (umd, esm, cjs, types) should be built when watching
+ */
+const watchOnly = ['umd'];
+
+const isDependency = (v) => Object.keys(pkg.dependencies || {}).some((e) => e === v || v.startsWith(e + '/'));
+const isPeerDependency = (v) => Object.keys(pkg.peerDependencies || {}).some((e) => e === v || v.startsWith(e + '/'));
+
+export default (options) => {
+  const buildFormat = (format) => {
+    return !options.watch || watchOnly.includes(format);
+  };
+  const commonOutput = {
+    sourcemap: true,
+    banner,
+    globals: {
+      'chart.js': 'Chart',
+      'chart.js/helpers': 'Chart.helpers',
+    },
+  };
+
+  const base = {
+    input: './src/index.ts',
+    external: (v) => isDependency(v) || isPeerDependency(v),
+    plugins: [
+      json(),
+      typescript(),
+      resolve({
+        mainFields: ['module', 'main'],
+        extensions: ['.mjs', '.cjs', '.js', '.jsx', '.json', '.node'],
+        modulesOnly: true,
+      }),
+      commonjs(),
+      replace({
+        preventAssignment: true,
+        values: {
+          // eslint-disable-next-line no-undef
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) || 'production',
+          __VERSION__: JSON.stringify(pkg.version),
+        },
+      }),
+      cleanup({
+        comments: ['some', 'ts', 'ts3s'],
+        extensions: ['ts', 'tsx', 'js', 'jsx'],
+        include: './src/**/*',
+      }),
+    ],
+  };
+  return [
+    buildFormat('esm') && {
+      ...base,
+      output: {
+        ...commonOutput,
+        file: pkg.module,
+        format: 'esm',
+      },
+    },
+    buildFormat('cjs') && {
+      ...base,
+      output: {
+        ...commonOutput,
+        file: pkg.main,
+        format: 'cjs',
+      },
+    },
+    (buildFormat('umd') || buildFormat('umd-min')) && {
+      ...base,
+      input: './src/index.umd.ts',
+      output: [
+        buildFormat('umd') && {
+          ...commonOutput,
+          file: pkg.unpkg.replace('.min', ''),
+          format: 'umd',
+          name: pkg.global,
+        },
+        buildFormat('umd-min') && {
+          ...commonOutput,
+          file: pkg.unpkg,
+          format: 'umd',
+          name: pkg.global,
+          plugins: [terser()],
+        },
+      ].filter(Boolean),
+      external: (v) => isPeerDependency(v),
+    },
+    buildFormat('types') && {
+      ...base,
+      output: {
+        ...commonOutput,
+        file: pkg.types,
+        format: 'es',
+      },
+      plugins: [
+        dts({
+          respectExternal: true,
+        }),
+      ],
+    },
+  ].filter(Boolean);
+};
